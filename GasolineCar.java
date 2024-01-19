7488a72d2eb9fe298abd1d0e2bc8326be63106ff
benteneh <153156826+benteneh@users.noreply.github.com>
2024-01-19 11:02:01 -0500

    Update GasolineCar.java

--- GasolineCar.java
+++ GasolineCar.java
@@ -1,1 +1,25 @@
+public class GasolineCar extends Car {
+    private double gasTankSize;
 
+    public GasolineCar() {
+        super();
+        this.gasTankSize = -1;
+    }
+
+    public GasolineCar(String makeAndModel, int numberOfDoors, int maximumNumberOfPassengers, double gasTankSize) {
+        super(makeAndModel, numberOfDoors, maximumNumberOfPassengers);
+        this.gasTankSize = gasTankSize;
+    }
+
+    public void setGasTankSize(double gasTankSize) {
+        this.gasTankSize = gasTankSize;
+    }
+
+    public double getGasTankSize() {
+        return this.gasTankSize;
+    }
+    @Override
+        public String toString() {
+            return "Gas tank size:\n" + gasTankSize;
+        }
+}
